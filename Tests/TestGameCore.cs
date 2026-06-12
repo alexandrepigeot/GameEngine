1ec29a42e146a6b75afb5ebe29524dd03306e646
Alexandre Pigeot <alexandrepigeot@gmail.com>
2026-06-12 10:27:57 +0400

    changed test suite

---
+++ Tests/TestGameCore.cs
@@ -0,0 +1,17 @@
+﻿using GameEngine;
+
+namespace Tests;
+
+[TestClass]
+public sealed class TestGameCore
+{
+	[TestMethod]
+	public void TestConstructor()
+	{
+		using var game1 = new GameCore("game1");
+
+		game1.RunOneFrame();
+
+		Assert.IsTrue(game1.IsActive);
+	}
+}
